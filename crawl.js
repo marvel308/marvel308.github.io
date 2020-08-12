785a639a1b2e9bbb5a4483b1af6b43794ad6328c
marvel308 <vtrivedi190@gmail.com>
2020-08-12 14:31:46 +0000

    Run ESLint

--- crawl.js
+++ crawl.js
@@ -1,73 +1,75 @@
-const fetch = require("node-fetch");
+const fetch = require('node-fetch');
 const fs = require('fs');
-const jsdom = require("jsdom");
+const jsdom = require('jsdom');
 
-const writeCriticalData = require('./critical.js')
+const writeCriticalData = require('./critical.js');
 
 function getEvents(document, event) {
-  result = []
-  for (let div of document.getElementsByClassName(event)) {
-      if(div.getElementsByClassName('event_icon own_goal').length > 0) {
-          let time = div.children[0].textContent.trim().split('\n')[0]
-          let goal_scorer = div.children[1].children[1].children[0].textContent.trim()
-          let fact = div.children[1].children[1].children[1].textContent.split('\n').map(a => a.trim()).reduce((a, b) => a + b)
-          result.push([time, goal_scorer, fact])
-      }
+  result = [];
+  for (const div of document.getElementsByClassName(event)) {
+    if (div.getElementsByClassName('event_icon own_goal').length > 0) {
+      const time = div.children[0].textContent.trim().split('\n')[0];
+      const goal_scorer = div.children[1].children[1].children[0].textContent.trim();
+      const fact = div.children[1].children[1].children[1].textContent.split('\n').map((a) => a.trim()).reduce((a, b) => a + b);
+      result.push([time, goal_scorer, fact]);
+    }
 
-      if(div.getElementsByClassName('event_icon goal').length > 0) {
-          let time = div.children[0].textContent.trim().split('\n')[0]
-          let goal_scorer = div.children[1].children[1].children[0].textContent.trim()
-          let fact = ''
-          if (div.children[1].children[1].children.length > 1)
-          fact = div.children[1].children[1].children[1].textContent.split('\n').map(a => a.trim()).reduce((a, b) => a + b)
-          result.push([time, goal_scorer, fact])
+    if (div.getElementsByClassName('event_icon goal').length > 0) {
+      const time = div.children[0].textContent.trim().split('\n')[0];
+      const goal_scorer = div.children[1].children[1].children[0].textContent.trim();
+      let fact = '';
+      if (div.children[1].children[1].children.length > 1) {
+        fact = div.children[1].children[1].children[1].textContent.split('\n').map((a) => a.trim()).reduce((a, b) => a + b);
       }
-      if(div.getElementsByClassName('event_icon penalty_goal').length > 0) {
-          let time = div.children[0].textContent.trim().split('\n')[0]
-          let goal_scorer = div.children[1].children[1].children[0].textContent.trim()
-          let fact = ''
-          if (div.children[1].children[1].children.length > 1)
-          fact = div.children[1].children[1].children[1].textContent.split('\n').map(a => a.trim()).reduce((a, b) => a + b)
-          result.push([time, goal_scorer, fact])
+      result.push([time, goal_scorer, fact]);
+    }
+    if (div.getElementsByClassName('event_icon penalty_goal').length > 0) {
+      const time = div.children[0].textContent.trim().split('\n')[0];
+      const goal_scorer = div.children[1].children[1].children[0].textContent.trim();
+      let fact = '';
+      if (div.children[1].children[1].children.length > 1) {
+        fact = div.children[1].children[1].children[1].textContent.split('\n').map((a) => a.trim()).reduce((a, b) => a + b);
       }
+      result.push([time, goal_scorer, fact]);
+    }
   }
-  return result
+  return result;
 }
 
 function getMatchFacts(document) {
-  let home = getEvents(document, 'event a')
-  let away = getEvents(document, 'event b')
-  return {home: home, away: away}
+  const home = getEvents(document, 'event a');
+  const away = getEvents(document, 'event b');
+  return {home: home, away: away};
 }
 
 function GetAllGames(document) {
-  let div = document.getElementById('sched_ks_3232_1')
-  let hrefss = []
-  for(let i = 0; i< div.rows.length; i++) {
-      if(div.rows[i].cells[12].firstElementChild) {
-        let result = [
-          div.rows[i].cells[2].textContent,
-          div.rows[i].cells[4].textContent,
-          div.rows[i].cells[8].textContent,
-          div.rows[i].cells[12].firstElementChild.href
-        ]
-          hrefss.push(result)
-      }
+  const div = document.getElementById('sched_ks_3232_1');
+  const hrefss = [];
+  for (let i = 0; i< div.rows.length; i++) {
+    if (div.rows[i].cells[12].firstElementChild) {
+      const result = [
+        div.rows[i].cells[2].textContent,
+        div.rows[i].cells[4].textContent,
+        div.rows[i].cells[8].textContent,
+        div.rows[i].cells[12].firstElementChild.href,
+      ];
+      hrefss.push(result);
+    }
   }
-  return hrefss
+  return hrefss;
 }
 
 async function fetchFromUrl(url) {
   url = 'https://fbref.com' + url;
-  let result = await fetch(url).then(function (response) {
+  const result = await fetch(url).then(function(response) {
     // The API call was successful!
     return response.text();
-  }).then(function (html) {
+  }).then(function(html) {
     // Convert the HTML string into a document object
     const dom = new jsdom.JSDOM(html);
- 
+
     return dom.window.document;
-  }).catch(function (err) {
+  }).catch(function(err) {
     // There was an error
     console.info('Something went wrong.', err);
   });
@@ -75,33 +77,33 @@ async function fetchFromUrl(url) {
 }
 
 async function fetchGame(arr) {
-  let doc = await fetchFromUrl(arr[3])
-  let game_data = getMatchFacts(doc)
-  let result = []
-  for(let game of game_data.home) {
-    result.push([arr[0], arr[1], arr[2], game[0], game[1], game[2], arr[1]].join(','))
+  const doc = await fetchFromUrl(arr[3]);
+  const game_data = getMatchFacts(doc);
+  const result = [];
+  for (const game of game_data.home) {
+    result.push([arr[0], arr[1], arr[2], game[0], game[1], game[2], arr[1]].join(','));
   }
-  for(let game of game_data.away) {
-    result.push([arr[0], arr[1], arr[2], game[0], game[1], game[2], arr[2]].join(','))
+  for (const game of game_data.away) {
+    result.push([arr[0], arr[1], arr[2], game[0], game[1], game[2], arr[2]].join(','));
   }
-  return result.join('\n')
+  return result.join('\n');
 }
 
 async function scrapeAllData(url) {
-	let result = []
-	let doc = await fetchFromUrl(url)
-	for (let game of GetAllGames(doc)) {
- 		let data =  await fetchGame(game)
- 		result.push(data)
+  const result = [];
+  const doc = await fetchFromUrl(url);
+  for (const game of GetAllGames(doc)) {
+ 		const data = await fetchGame(game);
+ 		result.push(data);
  	}
- 	return result.filter(x => x).join('\n')
+ 	return result.filter((x) => x).join('\n');
 }
 
 async function writeData() {
-	data = await scrapeAllData('/en/comps/9/schedule/Premier-League-Scores-and-Fixtures');
-	data = ['Date,Home,Away,Minute Scored,Goal Scorer,Fact,Team', data].join('\n')
-	fs.writeFile('data.csv', data, (err) => console.error(err))
-    writeCriticalData(data)
+  data = await scrapeAllData('/en/comps/9/schedule/Premier-League-Scores-and-Fixtures');
+  data = ['Date,Home,Away,Minute Scored,Goal Scorer,Fact,Team', data].join('\n');
+  fs.writeFile('data.csv', data, (err) => console.error(err));
+  writeCriticalData(data);
 }
 
-writeData()
+writeData();
--- critical.js
+++ critical.js
@@ -32,75 +32,71 @@ function compare2(a, b) {
 
 
 function insertIntoMap(map, value) {
-	// value = value.replace(/"/g, '')
-	if(map.has(value)) {
-		map.set(value, map.get(value) + 1)
-	}
-	else {
-		map.set(value, 1)
-	}
+  // value = value.replace(/"/g, '')
+  if (map.has(value)) {
+    map.set(value, map.get(value) + 1);
+  } else {
+    map.set(value, 1);
+  }
 }
 
 function GetDataFromMap(map, map2) {
-	let result = []
-	for (let [key, value] of map) {
-		result.push([value, map2.get(key), key])
-	}
-	result.sort((a, b) => {
-		if(parseInt(a[0]) < parseInt(b[0])) {
-			return -1
-		}
-		if(parseInt(a[0]) > parseInt(b[0])) {
-			return 1
-		}
-		return 0
-	})
-	return result.reverse().map(a => a.join(',')).join('\n')
+  const result = [];
+  for (const [key, value] of map) {
+    result.push([value, map2.get(key), key]);
+  }
+  result.sort((a, b) => {
+    if (parseInt(a[0]) < parseInt(b[0])) {
+      return -1;
+    }
+    if (parseInt(a[0]) > parseInt(b[0])) {
+      return 1;
+    }
+    return 0;
+  });
+  return result.reverse().map((a) => a.join(',')).join('\n');
 }
 
 function getCriticalData(data) {
-	let m = new Map()
+  const m = new Map();
 
-	let critical_goals = []
+  const critical_goals = [];
 
-	data.forEach((a) => {
-		let key = a[1] + "-" + a[2];
-		let delta = 0;
-		if (a[1] == a[6]) {
-			delta++
-		}
-		else if (a[2] == a[6]) {
-			delta--
-		}
-		if (m.has(key)) {
-			let score = m.get(key)
-			m.set(key, score + delta)
-		}
-		else {
-			m.set(key, delta)
-		}
-		if (a[1] == a[6]) {
-			if(m.get(key) >=0 && m.get(key) <=2) {
-				critical_goals.push(a)
-			}
-		}
-		else if (a[2] == a[6]) {
-			if(m.get(key) <=0 && m.get(key) >=-2) {
-				critical_goals.push(a)
-			}
-		}
-	})
-	critical_goals.sort(compare2)
-	return critical_goals.map(x => x.join(',')).join('\n')
+  data.forEach((a) => {
+    const key = a[1] + '-' + a[2];
+    let delta = 0;
+    if (a[1] == a[6]) {
+      delta++;
+    } else if (a[2] == a[6]) {
+      delta--;
+    }
+    if (m.has(key)) {
+      const score = m.get(key);
+      m.set(key, score + delta);
+    } else {
+      m.set(key, delta);
+    }
+    if (a[1] == a[6]) {
+      if (m.get(key) >=0 && m.get(key) <=2) {
+        critical_goals.push(a);
+      }
+    } else if (a[2] == a[6]) {
+      if (m.get(key) <=0 && m.get(key) >=-2) {
+        critical_goals.push(a);
+      }
+    }
+  });
+  critical_goals.sort(compare2);
+  return critical_goals.map((x) => x.join(',')).join('\n');
 }
 
 async function writeCriticalData(data) {
-	data = data.split('\n')
-	data = data.map(a => a.split(','))
-	data.sort(compare)
-	let critical_goals = getCriticalData(data)
-	let critical_data = ['Date,Home,Away,Minute Scored,Goal Scorer,Fact,Team', critical_goals].join('\n')
-	fs.writeFile('critical_data.csv', critical_data, (err) => console.error(err))
+  data = data.split('\n');
+  data = data.map((a) => a.split(','));
+  data.sort(compare);
+  const critical_goals = getCriticalData(data);
+  const critical_data = ['Date,Home,Away,Minute Scored,Goal Scorer,Fact,Team', critical_goals].join('\n');
+  fs.writeFile('critical_data.csv', critical_data, (err) => console.error(err));
 }
 
-module.exports = writeCriticalData
+module.exports = writeCriticalData;
